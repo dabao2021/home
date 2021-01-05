16f2884c71b1a1d1d7f0b3f2f3c6cc09b6e461ce
dabao2021 <349900176@qq.com>
2021-01-05 23:39:04 +0800

    xadmin增加板块

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -7,10 +7,20 @@ from users.models import Zuozhe
 
 
 class ArticleXadmin():
-    list_display = ['id', 'title', 'content', 'fine', 'sort', 'cli_num','name']
-    list_editable = ['title', 'content']
+    list_display = ['id', 'title', 'content', 'fine', 'sort', 'cli_num','check_status','zuozhe','update_time','create_time']
+    list_editable = ['title', 'content','check_status','zuozhe','update_time','create_time']
+    # search_fields = ['title', 'content']
+    model_icon = 'fa fa-th-list'
+    list_per_page = 10
+
+class ZuozheXadmin():
+    list_display = ['id', 'zuozheNo', 'name']
+    list_editable = ['zuozheNo', 'name']
+    model_icon = 'fa fa-address-book-o'  #'fa fa-user'
+
 
 xadmin.site.register(Article,ArticleXadmin)
+xadmin.site.register(Zuozhe,ZuozheXadmin)
 
 
 
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,25 +1,29 @@
 from django.db import models
-
-# Create your models here.
+from datetime import datetime
 
 class Zuozhe(models.Model):
     zuozheNo = models.CharField(max_length=15, verbose_name='作者编号', null=False, blank=False)
     name = models.CharField(max_length=20, verbose_name='作者姓名', null=False, blank=False)
+
     def __str__(self):
         return self.name
+
     class Meta():
         db_table = 'zuozhe'
         verbose_name = '作者'
         verbose_name_plural = verbose_name
 
-
 class Article(models.Model):
-    title = models.CharField(max_length=30,verbose_name='文章名',null=False,blank=False)
-    content = models.TextField(verbose_name='内容',default='')
-    fine = models.BooleanField(verbose_name='是否精华',default=False)
-    sort = models.IntegerField(verbose_name='顺序',default=0)
+    title = models.CharField(max_length=30, verbose_name='文章名', null=False, blank=False)
+    content = models.TextField(verbose_name='内容', default='')
+    fine = models.BooleanField(verbose_name='是否精华', default=False)
+    sort = models.IntegerField(verbose_name='顺序', default=0)
     cli_num = models.IntegerField(default=0, verbose_name='点击量')
-    zuozhe = models.ForeignKey(Zuozhe) #,related_name='id'
+    zuozhe = models.ForeignKey(Zuozhe, null=True, blank=True,verbose_name='作者')  # ,related_name='id'
+    check_status = models.IntegerField(default=0,verbose_name='审核状态',choices=((0,'审核中'),(1,'审核通过'),(2,'审核失败')))
+    create_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间') #default=datetime.now,
+    update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+
 
     def __str__(self):
         return self.title
