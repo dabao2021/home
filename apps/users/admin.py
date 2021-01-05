34aa39d7db1cba604f3b7f7c8a4b393316c081bc
dabao2021 <349900176@qq.com>
2021-01-05 18:01:10 +0800

    xadmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,11 +3,13 @@
 from django.db import models
 import xadmin
 from users.models import Article
+from users.models import Zuozhe
 
 
 class ArticleXadmin():
-    list_display = ['id','title']
-    list_editable = ['title',]
+    list_display = ['id', 'title', 'content', 'fine', 'sort', 'cli_num','name']
+    list_editable = ['title', 'content']
+
 xadmin.site.register(Article,ArticleXadmin)
 
 
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,16 @@
-# Create your models here.
 from django.db import models
 
+# Create your models here.
+
+class Zuozhe(models.Model):
+    zuozheNo = models.CharField(max_length=15, verbose_name='作者编号', null=False, blank=False)
+    name = models.CharField(max_length=20, verbose_name='作者姓名', null=False, blank=False)
+    def __str__(self):
+        return self.name
+    class Meta():
+        db_table = 'zuozhe'
+        verbose_name = '作者'
+        verbose_name_plural = verbose_name
 
 
 class Article(models.Model):
@@ -9,13 +19,12 @@ class Article(models.Model):
     fine = models.BooleanField(verbose_name='是否精华',default=False)
     sort = models.IntegerField(verbose_name='顺序',default=0)
     cli_num = models.IntegerField(default=0, verbose_name='点击量')
+    zuozhe = models.ForeignKey(Zuozhe) #,related_name='id'
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
 
-    class Meta:
+    class Meta():
         db_table = 'article'
-        verbose_name = '文章管理222'
+        verbose_name = '文章管理'
         verbose_name_plural = verbose_name
-
-
